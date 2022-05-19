50707bfd335f6a10fa606eaaa7a54392a44a093e
le sin <sin.letrung@gmail.com>
2022-05-19 15:02:20 +0700

    s

--- api/controllers/OptimzeController.js
+++ api/controllers/OptimzeController.js
@@ -3,42 +3,39 @@
 const util = require('util')
 const mysql = require('mysql')
 const db = require('./../db')
-const compress = require('./compress')
+const { compress } = require('compress-images/promise');
 const rootInput = "/var/www/compress-images.com/node/originalfiles/"
 const rootOutput = "/var/www/compress-images.com/node/optimalfile/"
 
 module.exports = {
-    opt: async (req, res) => {
+    opt: (req, res) => {
         let input = req.params.input;
-        //res.json(compress.compress_image(rootInput + input,rootOutput));
-        let result =  await compress.compress_image(rootInput + input,rootOutput);
-        res.json(result.input);
-        // const processImages = async (onProgress) => {
-        //     const result = await compress({
-        //         source: rootInput + input,
-        //         destination: rootOutput,
-        //         onProgress,
-        //         enginesSetup: {
-        //             jpg: { engine: 'mozjpeg', command: ['-quality', '60']},
-        //             png: { engine: 'pngquant', command: ['--quality=20-50', '-o']},
-        //         }
-        //     });
+        const processImages = async (onProgress) => {
+            const result = await compress({
+                source: rootInput + input,
+                destination: rootOutput,
+                onProgress,
+                enginesSetup: {
+                    jpg: { engine: 'mozjpeg', command: ['-quality', '60']},
+                    png: { engine: 'pngquant', command: ['--quality=20-50', '-o']},
+                }
+            });
     
-        //     const { statistics, errors } = result;
-        //     // statistics - all processed images list
-        //     // errors - all errros happened list
-        // };
+            const { statistics, errors } = result;
+            // statistics - all processed images list
+            // errors - all errros happened list
+        };
     
-        // processImages((error, statistic, completed) => {
-        //     if (error) {
-        //         console.log('Error happen while processing file');
-        //         console.log(error);
-        //         throw error
-        //     }
-        //     console.log('Sucefully processed file');
-        //     console.log(statistic)
-        //     res.json(statistic) 
-        // });
+        processImages((error, statistic, completed) => {
+            if (error) {
+                console.log('Error happen while processing file');
+                console.log(error);
+                throw error
+            }
+            console.log('Sucefully processed file');
+            console.log(statistic)
+            res.json(statistic.input); 
+        });
     },
     opts: (req, res) => {
 
