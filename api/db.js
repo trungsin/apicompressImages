910b6a812997f7aed836350231773bf0aa6e7d26
le sin <sin.letrung@gmail.com>
2022-05-23 11:15:17 +0700

    g

--- api/db.js
+++ api/db.js
@@ -7,5 +7,8 @@ const db = mysql.createConnection({
   password: process.env.DB_PASS || "data-default",
   database: process.env.DB_NAME || "d"
 });
-
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
 module.exports = db
