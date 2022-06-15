0d4ec4d6ae402f3a84b9e8b29dd14cbe8222519d
le sin <sin.letrung@gmail.com>
2022-06-15 08:06:36 +0700

    s

--- api/mysql.js
+++ api/mysql.js
@@ -4,7 +4,7 @@ const util = require('util')
 const mysql = require('mysql')
 const db = require('./db')
 
-function updateOpt(originalfile_,size_in_,size_output_,percent_,imageID_){
+async function updateOpt(originalfile_,size_in_,size_output_,percent_,imageID_){
   var sqli = "UPDATE product_images SET optimalfile = '"+originalfile_+"',timeoptimal=1, originalsize='"+size_in_+"', optimalsize='"+size_output_+"',percent='"+percent_+"' WHERE imageID = '"+imageID_+"'";
   db.query(sqli, function (err, resulti) {
     if (err) throw err;
