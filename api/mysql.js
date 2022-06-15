7902fd640b97c2ebcae5f1e68e4473dc695c4df3
le sin <sin.letrung@gmail.com>
2022-06-15 08:30:50 +0700

    s

--- api/mysql.js
+++ api/mysql.js
@@ -7,7 +7,10 @@ const db = require('./db')
 function updateOpt(originalfile_,size_in_,size_output_,percent_,imageID_){
   var sqli = "UPDATE product_images SET optimalfile = '"+originalfile_+"',timeoptimal=1, originalsize='"+size_in_+"', optimalsize='"+size_output_+"',percent='"+percent_+"' WHERE imageID = '"+imageID_+"'";
   db.query(sqli, function (err, resulti) {
-    //if (err) throw err;
+    if (err){
+      console.log(err);
+      return false;
+    } 
     console.log(resulti.affectedRows + " record(s) updated");
   });
 }
